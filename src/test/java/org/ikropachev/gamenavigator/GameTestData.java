aa9a8897a85db15961f978ee8f05d1d152a617d3
Rock133 <paporotnik796@gmail.com>
2022-03-24 06:26:25 +0300

    update_game_and_genre_test_data

--- src/test/java/org/ikropachev/gamenavigator/GameTestData.java
+++ src/test/java/org/ikropachev/gamenavigator/GameTestData.java
@@ -1,8 +1,29 @@
 package org.ikropachev.gamenavigator;
 
+import org.ikropachev.gamenavigator.model.Game;
+
+import java.util.List;
+
 import static org.ikropachev.gamenavigator.model.AbstractBaseEntity.START_SEQ;
 
 public class GameTestData {
+    public static final MatcherFactory.Matcher<Game> GAME_MATCHER = MatcherFactory.usingEqualsComparator(Game.class);
 
     public static final int GAME_ID = START_SEQ + 8;
+
+    public static final Game game1 = new Game(GAME_ID, "half-life");
+    public static final Game game2 = new Game(GAME_ID + 1, "silent hill");
+    public static final Game game3 = new Game(GAME_ID + 2, "warcraft");
+    public static final Game game4 = new Game(GAME_ID + 3, "starcraft");
+
+    //Games must be sorted by name
+    public static final List<Game> games = List.of(game1, game2, game4, game3);
+
+    public static Game getNew() {
+        return new Game(null, "New_Game");
+    }
+
+    public static Game getUpdated() {
+        return new Game(GAME_ID, "Updated_Game");
+    }
 }
--- src/test/java/org/ikropachev/gamenavigator/GenreTestData.java
+++ src/test/java/org/ikropachev/gamenavigator/GenreTestData.java
@@ -1,8 +1,29 @@
 package org.ikropachev.gamenavigator;
 
+import org.ikropachev.gamenavigator.model.Genre;
+
+import java.util.List;
+
 import static org.ikropachev.gamenavigator.model.AbstractBaseEntity.START_SEQ;
 
 public class GenreTestData {
+    public static final MatcherFactory.Matcher<Genre> GENRE_MATCHER = MatcherFactory.usingEqualsComparator(Genre.class);
 
-    public static final int GAME_ID = START_SEQ + 4;
+    public static final int GENRE_ID = START_SEQ + 4;
+
+    public static final Genre genre1 = new Genre(GENRE_ID, "action");
+    public static final Genre genre2 = new Genre(GENRE_ID + 1, "rts");
+    public static final Genre genre3 = new Genre(GENRE_ID + 2, "adventure");
+    public static final Genre genre4 = new Genre(GENRE_ID + 3, "horror");
+
+    //Genres must be sorted by name
+    public static final List<Genre> genres = List.of(genre1, genre3, genre4, genre2);
+
+    public static Genre getNew() {
+        return new Genre(null, "New_Genre");
+    }
+
+    public static Genre getUpdated() {
+        return new Genre(GENRE_ID, "Updated_Genre");
+    }
 }
