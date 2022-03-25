7149e2b014fc44f137fa3851faa335306ef86daa
Rock133 <paporotnik796@gmail.com>
2022-03-25 23:56:56 +0300

    update_game_genre_models

--- src/main/java/org/ikropachev/gamenavigator/model/Game.java
+++ src/main/java/org/ikropachev/gamenavigator/model/Game.java
@@ -2,11 +2,10 @@ package org.ikropachev.gamenavigator.model;
 
 import io.swagger.annotations.ApiModelProperty;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 @Table(name = "game")
@@ -18,12 +17,19 @@ public class Game extends AbstractNamedEntity {
     @ApiModelProperty(example = "developer")
     protected String developer;
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "game_x_genre",
+            joinColumns = @JoinColumn(name = "game_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id"))
+    private List<Genre> genres;
+
     public Game() {
     }
 
-    public Game(Integer id, String name, String developer) {
+    public Game(Integer id, String name, String developer, List<Genre> genres) {
         super(id, name);
         this.developer = developer;
+        this.genres = genres;
     }
 
     public String getDeveloper() {
@@ -32,5 +38,13 @@ public class Game extends AbstractNamedEntity {
 
     public void setDeveloper(String developer) {
         this.developer = developer;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
     }
 }
--- src/main/java/org/ikropachev/gamenavigator/model/Genre.java
+++ src/main/java/org/ikropachev/gamenavigator/model/Genre.java
@@ -1,16 +1,31 @@
 package org.ikropachev.gamenavigator.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "genre")
 public class Genre extends AbstractNamedEntity {
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "genre")
+    @JsonIgnore
+    private List<Game> games;
+
     public Genre() {
     }
 
-    public Genre(Integer id, String name) {
+    public Genre(Integer id, String name, List<Game> games) {
         super(id, name);
+        this.games = games;
+    }
+
+    public List<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(List<Game> games) {
+        this.games = games;
     }
 }
