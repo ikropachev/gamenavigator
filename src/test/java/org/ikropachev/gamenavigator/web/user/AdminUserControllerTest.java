af456fbaf9a7455127d3ddb93ba5e8b702fff856
Rock133 <paporotnik796@gmail.com>
2022-03-29 21:43:50 +0300

    not_found_user_tests_fixed

--- src/test/java/org/ikropachev/gamenavigator/web/user/AdminUserControllerTest.java
+++ src/test/java/org/ikropachev/gamenavigator/web/user/AdminUserControllerTest.java
@@ -38,10 +38,14 @@ public class AdminUserControllerTest extends AbstractControllerTest {
 
     @Test
     void getNotFound() throws Exception {
+        /*
         perform(MockMvcRequestBuilders.get(REST_URL + NOT_FOUND)
                 .with(userHttpBasic(admin)))
                 .andDo(print())
                 .andExpect(status().isUnprocessableEntity());
+
+         */
+        assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND));
     }
 
     @Test
@@ -64,10 +68,13 @@ public class AdminUserControllerTest extends AbstractControllerTest {
 
     @Test
     void deleteNotFound() throws Exception {
+        /*
         perform(MockMvcRequestBuilders.delete(REST_URL + NOT_FOUND)
                 .with(userHttpBasic(admin)))
                 .andDo(print())
                 .andExpect(status().isUnprocessableEntity());
+         */
+        assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND));
     }
 
     @Test
