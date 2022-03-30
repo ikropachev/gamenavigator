e0fa2483802480ae34534e8096c96759ac1c1d11
Rock133 <paporotnik796@gmail.com>
2022-03-30 23:43:21 +0300

    user_game_controller_test_complited

--- src/test/java/org/ikropachev/gamenavigator/web/game/UserGameControllerTest.java
+++ src/test/java/org/ikropachev/gamenavigator/web/game/UserGameControllerTest.java
@@ -1,31 +1,25 @@
 package org.ikropachev.gamenavigator.web.game;
 
-import org.ikropachev.gamenavigator.model.Game;
 import org.ikropachev.gamenavigator.service.GameService;
-import org.ikropachev.gamenavigator.util.exception.NotFoundException;
 import org.ikropachev.gamenavigator.web.AbstractControllerTest;
-import org.ikropachev.gamenavigator.web.json.JsonUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.ikropachev.gamenavigator.GameTestData.*;
-import static org.ikropachev.gamenavigator.GameTestData.GAME_ID;
 import static org.ikropachev.gamenavigator.TestUtil.userHttpBasic;
 import static org.ikropachev.gamenavigator.UserTestData.admin;
 import static org.ikropachev.gamenavigator.UserTestData.user1;
-import static org.ikropachev.gamenavigator.model.AbstractBaseEntity.NOT_FOUND;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.ikropachev.gamenavigator.web.game.AdminGameController.GENRE_NAME;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class UserGameControllerTest extends AbstractControllerTest {
     private static final String REST_URL = UserGameController.REST_URL;
 
-    @Autowired
-    private GameService service;
+    //@Autowired
+    //private GameService service;
 
     @Test
     void getAll() throws Exception {
@@ -34,5 +28,14 @@ public class UserGameControllerTest extends AbstractControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(GAME_MATCHER.contentJson(game1, game2, game4, game3));
+    }
+
+    @Test
+    void getAllByGenre() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "by-genre?genre=" + GENRE_NAME)
+                .with(userHttpBasic(admin)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(GAME_MATCHER.contentJson(game1, game2));
     }
 }
