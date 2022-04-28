4a291f4e5c88abe1db8c6b30711f53ee4956738d
Rock133 <paporotnik796@gmail.com>
2022-04-28 22:09:15 +0300

    update_GameTestData_(add_genres_lists,_but_ыешдд_ignoring_in_tests)

--- src/test/java/org/ikropachev/gamenavigator/GameTestData.java
+++ src/test/java/org/ikropachev/gamenavigator/GameTestData.java
@@ -6,6 +6,7 @@ import org.ikropachev.gamenavigator.model.Genre;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.ikropachev.gamenavigator.GenreTestData.*;
 import static org.ikropachev.gamenavigator.model.AbstractBaseEntity.START_SEQ;
 
 public class GameTestData {
@@ -14,10 +15,10 @@ public class GameTestData {
 
     public static final int GAME_ID = START_SEQ + 8;
 
-    public static final Game game1 = new Game(GAME_ID, "half-life", "valve");
-    public static final Game game2 = new Game(GAME_ID + 1, "silent hill", "konami");
-    public static final Game game3 = new Game(GAME_ID + 2, "warcraft", "blizzard");
-    public static final Game game4 = new Game(GAME_ID + 3, "starcraft", "blizzard");
+    public static final Game game1 = new Game(GAME_ID, "half-life", "valve", Arrays.asList(genre1, genre4));
+    public static final Game game2 = new Game(GAME_ID + 1, "silent hill", "konami", Arrays.asList(genre1, genre3, genre4));
+    public static final Game game3 = new Game(GAME_ID + 2, "warcraft", "blizzard", Arrays.asList(genre2));
+    public static final Game game4 = new Game(GAME_ID + 3, "starcraft", "blizzard", Arrays.asList(genre2));
 
     //Games must be sorted by name
     public static final List<Game> games = List.of(game1, game2, game4, game3);
